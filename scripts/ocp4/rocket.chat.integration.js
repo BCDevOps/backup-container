57e87eb836a37daf42b6aae6f0864b4d1ef10bfe
Wade Barnes <wade.barnes@shaw.ca>
2021-01-05 09:07:03 -0800

    Add ocp4 version of the rocket.chat integration script
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ scripts/ocp4/rocket.chat.integration.js
@@ -0,0 +1,50 @@
+class Script {
+  /**
+   * @params {object} request
+   */
+  process_incoming_request({ request }) {
+    let data = request.content;
+    let attachmentColor = `#36A64F`;
+    let statusMsg = `Status`;
+    let isError = data.statusCode === `ERROR`;
+    if (isError) {
+      statusMsg = `Error`;
+      attachmentColor = `#A63636`;
+    }
+
+    let friendlyProjectName=``;
+    if(data.projectFriendlyName) {
+      friendlyProjectName = data.projectFriendlyName
+    }
+
+    let projectName=``;
+    if (data.projectName) {
+      projectName = data.projectName
+      if(!friendlyProjectName) {
+        friendlyProjectName = projectName
+      }
+    }
+
+    if(projectName) {
+      statusMsg += ` message received from [${friendlyProjectName}](https://console.apps.silver.devops.gov.bc.ca/k8s/cluster/projects/${projectName}/workloads):`;
+    } else {
+      statusMsg += ` message received:`;
+    }
+
+    if (isError) {
+      statusMsg = `**${statusMsg}**`;
+    }
+
+    return {
+      content:{
+        text: statusMsg,
+        attachments: [
+          {
+            text: `${data.message}`,
+            color: attachmentColor
+          }
+        ]
+      }
+    };
+  }
+}
