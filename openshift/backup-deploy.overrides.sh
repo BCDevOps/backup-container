c33f88e7fa5a819ae87382bc875ce6c7386a3508
Emiliano Suñé <emiliano.sune@gmail.com>
2021-02-18 17:13:33 -0800

    Add missing function import
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/backup-deploy.overrides.sh
+++ openshift/backup-deploy.overrides.sh
@@ -3,6 +3,7 @@ _includeFile=$(type -p overrides.inc)
 # Import ocFunctions.inc for getSecret
 _ocFunctions=$(type -p ocFunctions.inc)
 if [ ! -z ${_includeFile} ]; then
+  . ${_ocFunctions}
   . ${_includeFile}
 else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
